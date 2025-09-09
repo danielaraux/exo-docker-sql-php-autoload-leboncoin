79c4a282c814fc636c87e1dece2dd598efd02ef4
daniel <danielaraux@hotmail.com>
2025-09-09 11:52:40 +0200

    structure à finir

---
+++ src/public/index.php
@@ -0,0 +1,9 @@
+<?php
+// On va utiliser l'autoload pour que les chemins en \ soient utilisables en / et donc lisibles par l'archi de dossiers Linux.
+// On va utiliser le router pour piloter nos actions.
+require_once 'vendor/autoload.php';
+require_once 'router.php';
+
+
+// Test de l'affichage
+// require_once 'src/src/Views/home.php';
---
+++ src/src/Views/home.php
@@ -0,0 +1,49 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home-LeBonCoin-Like</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="/../src/assets/css/style.css">
+</head>
+
+<body>
+    <header>
+        <nav>
+            <div class="text-center bg-dark text-light py-2">
+                <h1>Home-LeBonCoin-Like</h1>
+            </div>
+        </nav>
+    </header>
+
+    <main class="min-vh-100">
+
+
+        <!-- <div class="container my-5 ">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-4 justify-content-center">
+               
+            </div>
+        </div>
+
+        <div class="d-flex justify-content-end m-3">
+            <a href="#" class="btn btn-primary">Revenir en haut</a>
+        </div> -->
+
+    </main>
+
+    <footer class="text-center bg-dark text-light py-2">
+        <div>
+            <h1>Home-LeBonCoin-Like</h1>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+
+</html>
