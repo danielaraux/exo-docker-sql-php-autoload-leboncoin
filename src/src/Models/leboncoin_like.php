9c9fbec9f9cc8947beb1c49232b78be0daac5bce
daniel <danielaraux@hotmail.com>
2025-09-09 11:42:01 +0200

    structure

---
+++ src/src/Models/leboncoin_like.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.2
+ */
+
+/**
+ * Database `leboncoin_like`
+ */
+
+/* `leboncoin_like`.`Annonce` */
+$Annonce = array(
+  array('Annonce_Id' => '1','Annonce_prix' => '10','Annonce_titre' => 'Donut Vermicelles Multicolores','Annonce_description' => 'Vend un Donut de Collection en métal peint avec des vermicelles multicolores, pas sérieux s\'abstenir','Annonce_date' => '2025-09-09','Annonce_photo' => 'PhotoDuDonut.jpg','Utilisateur_Id' => '1'),
+  array('Annonce_Id' => '2','Annonce_prix' => '20','Annonce_titre' => 'Mégaphone','Annonce_description' => 'Vend un Mégaphone en état relatif, pas sérieux s\'abstenir','Annonce_date' => '2030-09-09','Annonce_photo' => 'PhotoDuMegaphone.jpg','Utilisateur_Id' => '1'),
+  array('Annonce_Id' => '3','Annonce_prix' => '5','Annonce_titre' => 'Tasse de café fissurée','Annonce_description' => 'Vend une Tasse de café fissurée, excellent pour la déco, pas sérieux s\'abstenir','Annonce_date' => '2031-09-09','Annonce_photo' => 'PhotoDeLaTasse.jpg','Utilisateur_Id' => '1'),
+  array('Annonce_Id' => '4','Annonce_prix' => '20','Annonce_titre' => 'PC Portable','Annonce_description' => 'PC Portable cassé','Annonce_date' => '2025-09-09','Annonce_photo' => 'PhotoPC','Utilisateur_Id' => '2'),
+  array('Annonce_Id' => '5','Annonce_prix' => '5','Annonce_titre' => 'Batte de baseball','Annonce_description' => 'Vend Batte de baseball usée','Annonce_date' => '2025-09-09','Annonce_photo' => 'PhotoBatte','Utilisateur_Id' => '2'),
+  array('Annonce_Id' => '6','Annonce_prix' => '15','Annonce_titre' => 'Chaussures de soirée','Annonce_description' => 'Vend chaussures de soirées en bon état','Annonce_date' => '2025-09-09','Annonce_photo' => 'PhotoChaussures','Utilisateur_Id' => '2')
+);
+
+/* `leboncoin_like`.`Utilisateur` */
+$Utilisateur = array(
+  array('Utilisateur_Id' => '1','Utilisateur_email' => 'homer.simpson@lol.com','Utilisateur_pseudo' => 'Dooh','Utilisateur_date' => '2025-09-09','Utilisateur_pass' => 'Ouhpinaise01'),
+  array('Utilisateur_Id' => '2','Utilisateur_email' => 'marge.simpson@lol.com','Utilisateur_pseudo' => 'Gemar','Utilisateur_date' => '2025-09-09','Utilisateur_pass' => 'Gemar001!')
+);
